b732f9ab0d65f7a56f119566478be45104c58c21
roman <romanantonov95@gmail.com>
2015-01-24 21:45:07 +0200

    Fixes 24.01.2015

--- hugo/scripts/main.js
+++ hugo/scripts/main.js
@@ -1,13 +1,4 @@
-$(window).load(function() {
-        // will first fade out the loading animation
-	$("#loader").fadeOut();
-        // will fade out the whole DIV that covers the website.
-	$("#loader-wrapper").delay(1000).fadeOut("slow");
-})
-	// $(window).load(function() { // makes sure the whole site is loaded
-	// 		$('#loader-wrapper').fadeOut(); // will first fade out the loading animation
 
-	// 	})
 $(document).ready(function() {
 	$(".add-to-cart").click(function(){
 		$(".product-image img").animate_from_to(".shopping-cart");
