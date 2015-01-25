9131b6b05e6f481acd1a333de19e81bf0383da6e
roman <romanantonov95@gmail.com>
2015-01-25 16:55:11 +0200

    commit 25.01.2015
    removed cdns libs

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,35 @@
 
 $(document).ready(function() {
+
+	//animation - add to cart
 	$(".add-to-cart").click(function(){
 		$(".product-image img").animate_from_to(".shopping-cart");
 		$(".non-active-cart").hide();
 		$(".active-cart").fadeIn();
 		$(".product-image img").animate_from_to(".active-cart");
 	})
-  //Click event to scroll to top
-  $('.scrollToTop').click(function(){
-    $('html, body').animate({scrollTop : 0},800);
-    return false;
-  });
-  
-  
+
+	  //Click event to scroll to top
+	  $('.scrollToTop').click(function(){
+	    $('html, body').animate({scrollTop : 0},800);
+	    return false;
+	  });
+	  
+
+	  //smooth scrolling 
+		$(function() {
+		  $('a.send_to_cart[href*=#]:not([href=#])').click(function() {
+		    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+		      var target = $(this.hash);
+		      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		      if (target.length) {
+		        $('html,body').animate({
+		          scrollTop: target.offset().top
+		        }, 1000);
+		        return false;
+		      }
+		    }
+		  });
+		});
 });
 
