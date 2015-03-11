60fa6e3f3d43568809048d2d37c788eb2e3868e8
Roman Antonov <romanantonov95@gmail.com>
2015-03-12 00:20:57 +0200

    changes 11.03.2015

--- Prepros Build/scripts/main.js
+++ Prepros Build/scripts/main.js
@@ -6,31 +6,30 @@ $(document).ready(function() {
         $('body').delay(350).css({'overflow':'visible'});
     })
 
-      if (!Modernizr.svg) {
-        $('img[src$=svg]').each(function(index, item) {
-            imagePath = $(item).attr('src');
-            $(item).attr('src',imagePath.slice(0,-3)+'png');
-        });
-    }
-	$(window).load(function(){
-		$( ".new_product_page" ).each(function() {
-	        var newHeight = 0, $this = $( this );
-	        $.each( $this.children(), function() {
-	            newHeight += $( this ).height();
-	        });
-	        $this.height( newHeight );
-	    });
+
+	// $(window).load(function(){
+	// 	$( ".new_product_page" ).each(function() {
+	//         var newHeight = 0, $this = $( this );
+	//         $.each( $this.children(), function() {
+	//             newHeight += $( this ).height();
+	//         });
+	//         $this.height( newHeight );
+	//     });
 		
-	});
-	//animation - add to cart
+	// });
+
+
+	//--------------animation - add to cart
 	$(".add-to-cart").click(function(){
 		$(".product-image img").animate_from_to(".shopping-cart");
 		$(".non-active-cart").hide();
 		$(".active-cart").fadeIn();
 		$(".product-image img").animate_from_to(".active-cart");
 	})
+	//--------------animation - add to cart
 
-	  //Click event to scroll to top
+
+	  //--------------Click event to scroll to top
 	  $('.scrollToTop').click(function(){
 	    $('html, body').animate({scrollTop : 0},800);
 	    return false;
@@ -42,15 +41,39 @@ $(document).ready(function() {
 	  	$(".carousel").swipeleft(function() {  
 	      $(this).carousel('next');  
 			});  
-	  	
-		var height = 0;
-		$('.new_products_spotlight ul li').each(function() {
-		    height = Math.max( height, $(this).height());
-		}).each(function() {
-		    $(this).height(height);
-		});
+	  //--------------Click event to scroll to top
 
-	  //smooth scrolling 
+
+	  //--------------home page tiles 
+		function setTheHeight() {
+
+		    if( $('.new_products_spotlight ul li .description ').length ) {
+		        //Get value of highest element
+		        var maxHeight = Math.max.apply(Math, $('.new_products_spotlight ul li .description').map (
+		            function() {
+		                var totalHeight = 0;
+		                
+		                $(this).children().each(function() {
+		                   totalHeight += $(this).outerHeight(); 
+		                });
+		                
+		                return totalHeight;
+		            }
+		        ));
+		        console.log(maxHeight);
+		        $('.new_products_spotlight ul li .description').height(maxHeight);
+		    }   
+		}
+
+		setTheHeight();
+
+		$(window).resize(function() {
+		    setTheHeight();
+		});
+	//--------------home page tiles
+
+
+	//--------------smooth scrolling 
 		$(function() {
 		  $('a.send_to_cart[href*=#]:not([href=#])').click(function() {
 		    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -65,9 +88,10 @@ $(document).ready(function() {
 		    }
 		  });
 		});
+	//--------------smooth scrolling
 
 
-
+	//--------------navbar click-popover
 	$(function(){
 	  $("#cart-link").popover({
 	    html: 'true',
@@ -76,4 +100,6 @@ $(document).ready(function() {
 			}
 	  })
 	});
+	//--------------navbar click-popover
+
 });
